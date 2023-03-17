912d93a9d11bbe3c1476ee2fd0e085e28cc41afd
vijayalaxmishirnal01 <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2023-03-17 12:19:39 +0530

    Add files via upload

---
+++ Ifelseif.java
@@ -0,0 +1,18 @@
+class Ifelseif{
+public static void main(String args[])
+{
+String City="Solapur";
+if(City== "Pune"){
+System.out.println("City is Pune");
+}
+else if(City== "Delhi"){
+System.out.println("City is Delhi");
+}
+else if(City== "Mumbai"){
+System.out.println("City is Mumbai");
+}
+else{
+System.out.println("City is Solapur");
+}
+}
+}
