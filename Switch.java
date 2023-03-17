f5d27b0d6cf2e5579c8392ccdbf06a92df41d600
vijayalaxmishirnal01 <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2023-03-17 13:00:33 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,23 @@
+class Switch{
+public static void main(String args[])
+{
+int n=10;
+switch(n){
+case 4:
+System.out.println("number is 4");
+break;
+case 7:
+System.out.println("number is 7");
+break;
+case 10:
+System.out.println("number is 10");
+break;
+case 0:
+System.out.println("number is 0");
+break;
+
+default:
+System.out.println("number is not");
+}
+}
+} 
