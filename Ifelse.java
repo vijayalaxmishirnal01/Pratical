c1d4344d08c547eb5cbad0a169c3de7c2c475f08
vijayalaxmishirnal01 <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2023-03-17 12:09:24 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,14 @@
+class Ifelse{
+public static void main(String args[])
+{
+int a=10;
+int b=15;
+if(a+b < 10)
+{
+System.out.println("a+b is less than 10");
+}
+else{
+System.out.println("a+b is greater than 20");
+}
+}
+}
---
+++ Simpleif.java
@@ -0,0 +1,11 @@
+class Simpleif{
+public static void main(String args[])
+{
+int a=10;
+int b=15;
+if(a+b > 20)
+{
+System.out.println("a+b is greater than 20");
+}
+}
+}
