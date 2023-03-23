5b04fe8613152455259193aca9435a082bbc2cfc
vijayalaxmishirnal01 <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2023-03-23 13:50:35 +0530

    Add files via upload

---
+++ Foreachloop.java
@@ -0,0 +1,9 @@
+class Foreachloop{
+public static void main (String args[]){
+String[] names ={"Riya","Rohit","viju","Amit","Swata"};
+System.out.println("Printing the content of the Student Names:\n");
+for(String result:names){
+System.out.println(result);
+}
+}
+}
---
+++ ForloopEx.java
@@ -0,0 +1,8 @@
+class ForloopEx{
+public static void main(String args[]){
+int num=5;
+for (int i=1; i<=num; i++){
+System.out.println("Vijayalaxmi");
+}
+}
+}
