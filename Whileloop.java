cec06b22e0006108e633bba1173e606a90d5f9cb
vijayalaxmishirnal01 <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2023-03-23 13:47:53 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,10 @@
+class Whileloop{
+public static void main(String args[]){
+int i=0;
+System.out.println("10 Even Numbers:\n");
+while(i<=10){
+System.out.println(i);
+i=i+2;
+}
+}
+}
---
+++ Whileloop1.java
@@ -0,0 +1,10 @@
+class Whileloop1{
+public static void main(String args[]){
+int i=1;
+System.out.println("10 Odd Numbers:\n");
+while(i<=10){
+System.out.println(2*i-1);
+i++;
+}
+}
+}
