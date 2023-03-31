e7c7a13a27e8335e814bfa92382b9ee5cdf14fbc
vijayalaxmishirnal01 <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2023-03-31 23:38:40 +0530

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -1,9 +1,37 @@
-class Rectangle{
-public static void main(String args[])
-{
-int lenght=5;
-int breadth=6;
-int Area=lenght * breadth;
-System.out.println("The Area of Rectangle:"+ Area);
+package lab;
+
+class Rectangle {
+	private double length;
+    private double breadth;
+    
+    public Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    
+    public double getArea() {
+        return length * breadth;
+    }
+    
+    public void setLength(double length) {
+        this.length = length;
+    }
+    
+    public void setBreadth(double breadth) {
+        this.breadth = breadth;
+    }
+
+public class Rectangle_Test {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(20.0, 40.0);
+        double area = rect.getArea();
+        System.out.println("Area = " + area);
+        
+        rect.setLength(30.0);
+        rect.setBreadth(30.0);
+        double newArea = rect.getArea();
+        System.out.println("New area = " + newArea);
+    }
 }
 }
+
